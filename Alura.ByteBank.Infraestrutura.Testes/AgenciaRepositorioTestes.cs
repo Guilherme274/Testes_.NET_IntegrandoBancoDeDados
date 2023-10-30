60e02edfff6524ec76077973755732f25e430ef6
Guilherme Paixão dos Santos <paixaoguilherme79@gmail.com>
2023-10-29 21:00:44 -0300

    Adicionando Testes para o CRUD e trabalhando com Mocks e Stubs
    Mock é uma biblioteca que permite o desenvolvedor a utilizar repositórios fake e dados mockados, isso torna possível uma simulação de dados em memória. Isso tem como utilidade preservar a integridade de dados compartilhados e garante a execução dos testes de forma isolada. Os Stubs funcionam de maneira parecida com os Mocks, sua adição é que além de testar o comportamento, eles também verificam se ocorreu o resultado esperado.

---
+++ Alura.ByteBank.Infraestrutura.Testes/AgenciaRepositorioTestes.cs
@@ -0,0 +1,176 @@
+﻿using Alura.ByteBank.Dados.Repositorio;
+using Alura.ByteBank.Dominio.Entidades;
+using Alura.ByteBank.Dominio.Interfaces.Repositorios;
+using Alura.ByteBank.Infraestrutura.Testes.Servico;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Alura.ByteBank.Infraestrutura.Testes
+{
+    public class AgenciaRepositorioTestes
+    {
+        private readonly IAgenciaRepositorio _repositorio;
+        public ITestOutputHelper SaidaConsoleTeste { get; set; }
+
+        public AgenciaRepositorioTestes(ITestOutputHelper _saidaConsoleTeste)
+        {
+            SaidaConsoleTeste = _saidaConsoleTeste;
+            SaidaConsoleTeste.WriteLine("Construtor invocado");
+
+            var servico = new ServiceCollection();
+            servico.AddTransient<IAgenciaRepositorio, AgenciaRepositorio>();
+            var provedor = servico.BuildServiceProvider();
+
+            _repositorio = provedor.GetService<IAgenciaRepositorio>();
+
+        }
+
+        [Fact]
+        public void TestaObterTodasAsAgencias()
+        {
+
+            //Arrange
+            //Act
+            List<Agencia> lista = _repositorio.ObterTodos();
+
+            //Assert
+            Assert.NotNull(lista);
+            Assert.Equal(2, lista.Count);
+        }
+
+
+        [Fact]
+        public void TestaObterAgenciasPorId()
+        {
+            //Arrange
+            //Act
+            var agencia = _repositorio.ObterPorId(23);
+
+
+
+            //Assert
+            Assert.NotNull(agencia);
+        }
+
+        [Theory]
+        [InlineData(23)]
+        [InlineData(24)]
+        public void TestaObterAgenciaPorVariosId(int id)
+        {
+            //Arrange
+            //Act
+            var agencia = _repositorio.ObterPorId(id);
+
+
+
+            //Assert
+            Assert.NotNull(agencia);
+        }
+
+        [Fact]
+        public void TestaAtualizaNumeroAgencia()
+        {
+
+            //Arrange
+            var agencia = _repositorio.ObterPorId(23);
+            int novoNumeroAgencia = 213;
+            agencia.Numero = novoNumeroAgencia;
+
+            //Act
+            var atualizado = _repositorio.Atualizar(23,agencia);
+
+
+
+            //Assert
+            Assert.True(atualizado);
+
+        }
+
+        [Fact]
+        public void TestaInserirUmaNovaAgenciaNoBancoDeDados()
+        {
+            //Arrange 
+            var agencia = new Agencia()
+            {
+                Nome = "Agencia Central Coast City",
+                Identificador = Guid.NewGuid(),
+                Id = 1,
+                Endereco = "Rua Alexandre Fleming, 212",
+                Numero = 43
+
+            };
+
+
+
+            //Act
+            var retorno = _repositorio.Adicionar(agencia);
+
+            //Assert
+            Assert.True(retorno);
+
+
+        }
+
+        [Fact]
+        public void TestaRemoverInformacaoDoBancoDeDados()
+        {
+            //Arrange
+            //Act
+            var dadoExcluido = _repositorio.Excluir(1);
+
+            //Assert
+            Assert.True(dadoExcluido);
+        }
+
+        [Fact]
+        public void TestaExcecaoConsultaPorAgenciaPorId()
+        {
+            //Act
+            //Assert
+            Assert.Throws<FormatException>( () => _repositorio.ObterPorId(23));
+        }
+
+        [Fact]
+        public void TestaObterAgenciasMock()
+        {
+            //Arange
+            var bytebankRepositorioMock = new Mock<IByteBankRepositorio>();
+            var mock = bytebankRepositorioMock.Object;
+
+            //Act
+            var lista = mock.BuscarAgencias();
+
+            //Assert
+            bytebankRepositorioMock.Verify(b => b.BuscarAgencias());
+        }
+
+        [Fact]
+        public void TestaAdiconarAgenciaMock()
+        {
+            // Arrange
+            var agencia = new Agencia()
+            {
+                Nome = "Agência Amaral",
+                Identificador = Guid.NewGuid(),
+                Id = 4,
+                Endereco = "Rua Arthur Costa",
+                Numero = 6497
+            };
+
+            var repositorioMock = new ByteBankRepositorio();
+
+            //Act
+            var adicionado = repositorioMock.AdicionarAgencia(agencia);
+
+            //Assert
+            Assert.True(adicionado);
+        }
+    }
+}
+
--- Alura.ByteBank.Infraestrutura.Testes/ClienteRepositorioTestes.cs
+++ Alura.ByteBank.Infraestrutura.Testes/ClienteRepositorioTestes.cs
@@ -1,13 +1,20 @@
-using Alura.ByteBank.Dados.Repositorio;
+﻿using Alura.ByteBank.Dados.Repositorio;
 using Alura.ByteBank.Dominio.Entidades;
 using Alura.ByteBank.Dominio.Interfaces.Repositorios;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
 
-namespace ClienteRepositorioTestes
+namespace Alura.ByteBank.Infraestrutura.Testes
 {
     public class ClienteRepositorioTestes
     {
-        private readonly IClienteRepositorio _repositorio;
+
+        private readonly IClienteRepositorio? _repositorio;
         public ClienteRepositorioTestes()
         {
             var servico = new ServiceCollection();
@@ -35,12 +42,29 @@ namespace ClienteRepositorioTestes
         {
             //Arrange
             //Act
-            var cliente = _repositorio.ObterPorId(1);
+            var cliente = _repositorio.ObterPorId(29);
 
 
 
             //Assert
             Assert.NotNull(cliente);
         }
+
+        [Theory]
+        [InlineData(29)]
+        [InlineData(30)]
+        public void TestaObterClientePorVariosId(int id)
+        {
+            //Arrange
+            //Act
+            var cliente = _repositorio.ObterPorId(id);
+
+
+
+            //Assert
+            Assert.NotNull(cliente);
+        }
+
     }
 }
+
